b80e2b5e4c1f0000fc1b4f3723348b7c23728420
chenda <1106982578@qq.com>
2021-05-19 15:24:59 +0800

    success read GF3 L2 data and show sinclair RGB image

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ from mylib import mathlib
 save_dir = r'/home/csl/code/preprocess/tmp'
 
 if __name__ == '__main__':
-	path = r'/home/csl/code/preprocess/data/MGGF3jihuanBC20210427/GF3_KAS_QPSI_024808_W116.2_N37.0_20210427_L2_AHV_L20005617966'
+	path = r'/home/csl/code/preprocess/data/MGGF3jihuanBC20210427/GF3_KAS_QPSI_024808_W116.2_N37.3_20210427_L2_AHV_L20005617967'
 	s4 = psr.read_c3_GF3_L2(path, is_print=True)
 
 	# for ii in range(4):
@@ -29,10 +29,10 @@ if __name__ == '__main__':
 
 	# 	cv2.imwrite(osp.join(save_dir, f'{ii}_img.png'), (mathlib.min_max_map(data)*255).astype(np.uint8))
 
-	s4 = s4[:, 10000:10500, 10000:10500]
-	rgb = psr.rgb_by_s2(s4, type='sinclair', if_log=False)
+	# s4 = s4[:, ::4, ::4]
+	rgb = psr.rgb_by_s2(s4, type='sinclair', if_log=False, if_mask=True)
 	# plt.hist()
-	cv2.imwrite(osp.join(save_dir, f'sinclari.png'), cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB))
+	cv2.imwrite(osp.join(path, f'SinclairRGB.png'), cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB))
 
 
 
